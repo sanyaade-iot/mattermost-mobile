9c977914b2c9cadfcab3550e4a4bddb249823ab9
Elias Nahum <nahumhbl@gmail.com>
2017-12-20 19:17:36 -0300

    Fix unit test

--- test/setup.js
+++ test/setup.js
@@ -27,6 +27,9 @@ mockery.registerMock('react-native', {
             addEventListener: () => true,
             fetch: () => Promise.resolve(true)
         }
+    },
+    Platform: {
+        OS: 'ios'
     }
 });
 mockery.registerMock('react-native-device-info', {
